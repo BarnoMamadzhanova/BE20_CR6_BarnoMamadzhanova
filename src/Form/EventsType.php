220bebfd36c1a45f8570e365e6f4884fdaf3d02e
BarnoMamadzhanova <b@mamadzhanova.com>
2023-12-08 14:50:31 +0100

    creating db with crud and controller

---
+++ src/Form/EventsType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Events;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('date')
+            ->add('description')
+            ->add('image')
+            ->add('capacity')
+            ->add('email')
+            ->add('phone_number')
+            ->add('address')
+            ->add('website')
+            ->add('type')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Events::class,
+        ]);
+    }
+}
