51705bfd24b327570899d76379b626e87c33ebd8
BarnoMamadzhanova <b@mamadzhanova.com>
2023-12-10 03:35:37 +0100

    creating fileupload

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -10,17 +10,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\FileUploader;
+
 
 #[Route('/')]
 class EventsController extends AbstractController
 {
     #[Route('/', name: 'app_events_index', methods: ['GET'])]
-    public function index(EventsRepository $eventsRepository): Response
+    public function index(EventsRepository $eventsRepository, FileUploader $fileUploader): Response
     {
+        $events = $eventsRepository->findAll();
+        $imageFromUploader = [];
+        foreach ($events as $event) {
+            $imageName = $event->getImage(); 
+            if ($imageName) {
+                $imageFromUploader[$event->getId()] = $fileUploader->getTargetDirectory() . '/' . $imageName;
+            }
+        }
+
         return $this->render('events/events.html.twig', [
-            'events' => $eventsRepository->findAll(),
+            'events' => $events,
+            'imageFromUploader' => $imageFromUploader,
         ]);
     }
+
 
     #[Route('/type', name: 'app_events_filter_by_type', methods: ['GET'])]
     public function filterByType(Request $request, EventsRepository $eventsRepository): Response
@@ -40,22 +53,36 @@ class EventsController extends AbstractController
     }
 
     #[Route('/new', name: 'app_events_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
     {
         $event = new Events();
         $form = $this->createForm(EventsType::class, $event);
         $form->handleRequest($request);
 
+        $imageFromUploader = null; 
+
         if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->get('image')->getData();
+            if ($image) {
+                $imageName = $fileUploader->upload($image);
+                $event->setImage($imageName);
+                $imageFromUploader = $fileUploader->getTargetDirectory() . '/' . $imageName; 
+            }
             $entityManager->persist($event);
             $entityManager->flush();
+
+            $this->addFlash(
+                'notice',
+                'New event has been added!'
+            );
 
             return $this->redirectToRoute('app_events_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('events/new.html.twig', [
             'event' => $event,
-            'form' => $form,
+            'form' => $form->createView(),
+            'imageFromUploader' => $imageFromUploader, 
         ]);
     }
 
@@ -68,20 +95,30 @@ class EventsController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_events_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Events $event, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Events $event, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
     {
         $form = $this->createForm(EventsType::class, $event);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->get('image')->getData();
+            if ($image) {
+                $imageName = $fileUploader->upload($image);
+                $event->setImage($imageName);
+            }
             $entityManager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Event has been edited!'
+            );
 
             return $this->redirectToRoute('app_events_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('events/edit.html.twig', [
             'event' => $event,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
--- src/Form/EventsType.php
+++ src/Form/EventsType.php
@@ -6,22 +6,68 @@ use App\Entity\Events;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\Length;
 class EventsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('date')
-            ->add('description')
-            ->add('image')
-            ->add('capacity')
-            ->add('email')
-            ->add('phone_number')
-            ->add('address')
-            ->add('website')
-            ->add('type')
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+            ])
+            ->add('date', DateTimeType::class, [
+                'attr' => ['style' => 'margin-bottom:15px']
+            ])
+            ->add('description', TextType::class, [
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Task image',
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px'],
+                'mapped' => false,
+                'required' => true,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                           'image/png',
+                           'image/jpeg',
+                           'image/jpg',
+                       ],
+                       'mimeTypesMessage' => 'Please upload a valid image file',
+                    ])
+                ],
+            ])
+            ->add('capacity', IntegerType::class, [
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom: 15px'],
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom: 15px' ],
+            ])
+            ->add('phone_number', TextType::class, [
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom: 15px'],
+                'constraints' => [new Length(['max' => 20])],
+            ])
+            ->add('address', TextType::class, [
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+            ])
+            ->add('website', TextType::class, [
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => ['Art' => 'art', 'Dance' => 'dance', 'Fashion' => 'fashion', 
+                'Food' => 'food', 'Gaming' => 'gaming', 'Movie' => 'movie', 
+                'Music' => 'music', 'Science' => 'science', 'Sport' => 'sport', 'Theater' => 'theater'],
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+            ])
         ;
     }
 
